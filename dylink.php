2281f118d3752d6548bd477ec64f12c20051ddcb
kimsg1984 <kimsg1984@gmail.com>
2012-12-11 01:18:13 +0900

    링크파일 변경시 파일이 없으면 리스트를 재검색하도록 설정

--- dylink.php
+++ dylink.php
@@ -1,8 +1,29 @@
 <? 
-$title = $_GET['title'];
-$title = strtolower($title);
+
+function getfile($title) {
 $notefile = shell_exec("python ./dylink.py \"$title\"");
 $notefile = str_replace("\n", "", $notefile);
+return($notefile);
+}
+
+$title = $_GET['title'];
+$title = strtolower($title);
+$notefile=getfile($title);
+$notefile = "dornrrk";
+
+if(is_file("./notefile/$notefile")==false){
+
+	shell_exec('python ./mklist.py > ./notefile/list.txt');
+	$notefile=getfile($title);
+	
+	if(is_file("./notefile/$notefile")==false){
+
+		echo "there is no file $title";
+
+		}	
+
+}
+
 $check_empty = strlen("$notefile");
    
 	if ( $check_empty == 2 ) { 
@@ -15,11 +36,10 @@ $check_empty = strlen("$notefile");
 		$content = str_replace(">", "&#62;", $content);
 		$content = str_replace("\"", "&#34;", $content);
 		}  
-function edit() {
-	}
 ?>
    
 <? include('head.html')   // 해더 불러오기(타이틀을 넣기위해 닫지는 않는다) ?>
+
 <title><?echo $title?></title></head><body><div class="note" id="<? 
 echo $title ?>"><a name="<? echo $title ?>"></a><h1><? 
 echo $title ?></h1><form action="edit.php" method="POST"><input type="hidden" name="title" value="<?
@@ -28,6 +48,6 @@ echo $notefile?>"><input type="hidden" name="content" value="<?
 echo $content?>"><input type="submit" value="EDIT">
 </form><Script LANGUAGE="javascript"></script><?echo $content_html?></div>
 
-<!-- <HR NOSHADE>
-<A href="<?echo "notefile/$notefile" ?>">XMLfile</A> | <A href="list.txt">LIST</A> -->
+<HR NOSHADE>
+<!-- <A href="<?echo "notefile/$notefile" ?>">XMLfile</A> | <A href="list.txt">LIST</A> -->
 </body></html>
